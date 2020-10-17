58fdf115982160b346f832466b170f3d045962d9
Hamza <hamza.kaizar@thoughtworks.com>
2020-10-17 15:16:30 +0530

    [Hamza] Move functions to Point class

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,20 +4,16 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
+    public double distanceBetween(Point point) {
+        double xDistance = point.x - this.x;
+        double yDistance = point.y - this.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
+    public double directionTo(Point point) {
+        double xDistance = point.x - this.x;
+        double yDistance = point.y - this.y;
+        return Math.atan2(yDistance, xDistance);
     }
 
     public Point(double x, double y) {
